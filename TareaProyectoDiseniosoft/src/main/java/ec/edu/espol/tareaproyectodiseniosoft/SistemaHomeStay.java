391592569e96a0c0495dfff2d085a313d4180821
MARLO VALLE <mcvalle@espol.edu.ec>
2024-12-09 22:42:03 -0500

    Create SistemaHomeStay.java

---
+++ TareaProyectoDiseniosoft/src/main/java/ec/edu/espol/tareaproyectodiseniosoft/SistemaHomeStay.java
@@ -0,0 +1,37 @@
+package ec.edu.espol.tareaproyectodiseniosoft;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SistemaHomeStay {
+     private Notificador notificador;
+
+    public SistemaHomeStay(Notificador notificador) {
+        this.notificador = notificador;
+    }
+
+    public List<Propiedad> buscarPropiedades(String filtro) {
+        System.out.println("Buscando propiedades con filtro: " + filtro);
+        return new ArrayList<>();
+    }
+
+    public Reserva crearReserva(Huesped huesped, Unidad unidad, Date fechaInicio, Date fechaFin) {
+        System.out.println("Creando reserva para el huésped: " + huesped.getNombre());
+        return new Reserva(huesped, unidad, fechaInicio, fechaFin);
+    }
+
+    public boolean procesarPago(Reserva reserva, MetodoPago metodo, double monto) {
+        System.out.println("Procesando pago...");
+        boolean resultado = metodo.procesar(monto);
+        if (resultado) {
+            reserva.setEstado(EstadoPago.COMPLETO);
+            notificador.enviarNotificacion(reserva.getHuesped().getEmail(), "Pago completado.");
+        } else {
+            reserva.setEstado(EstadoPago.FALLIDO);
+            notificador.enviarNotificacion(reserva.getHuesped().getEmail(), "Pago fallido.");
+        }
+        return resultado;
+    }
+
+
+}
