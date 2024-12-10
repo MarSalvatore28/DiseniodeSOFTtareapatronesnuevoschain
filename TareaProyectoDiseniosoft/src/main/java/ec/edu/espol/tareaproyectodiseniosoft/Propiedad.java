58642f74ce4c5b5e331007181ff1a6d8e501d3c1
DhamarPatino <152041769+DhamarPatino@users.noreply.github.com>
2024-12-09 23:14:15 -0500

    clase propiedad y unidad

---
+++ TareaProyectoDiseniosoft/src/main/java/ec/edu/espol/tareaproyectodiseniosoft/Propiedad.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espol.tareaproyectodiseniosoft;
+
+import java.util.List;
+
+/**
+ *
+ * @author DHAMAR
+ */
+public class Propiedad {
+     private List<Unidad> unidades;
+
+    public List<Unidad> verificarDisponibilidad() {
+        List<Unidad> disponibles = new ArrayList<>();
+        for (Unidad unidad : unidades) {
+            if (unidad.getEstado() == EstadoUnidad.DISPONIBLE) {
+                disponibles.add(unidad);
+            }
+        }
+        return disponibles;
+    }
+
+}
