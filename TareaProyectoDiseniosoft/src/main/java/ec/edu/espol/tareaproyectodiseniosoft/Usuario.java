0a857b396ae7c99bf7b1397b8f74771ebee7ed07
MARLO VALLE <mcvalle@espol.edu.ec>
2024-12-09 22:46:14 -0500

    Create Usuario.java

---
+++ TareaProyectoDiseniosoft/src/main/java/ec/edu/espol/tareaproyectodiseniosoft/Usuario.java
@@ -0,0 +1,17 @@
+package ec.edu.espol.tareaproyectodiseniosoft;
+
+public abstract class Usuario {
+    private int id;
+    private String nombre;
+    private String email;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+
+}
