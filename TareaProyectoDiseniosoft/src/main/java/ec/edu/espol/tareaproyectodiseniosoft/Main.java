3d1d55e10f6e57bdaf81f1631cd754ae446c385f
MARLO VALLE <mcvalle@espol.edu.ec>
2024-12-09 23:49:04 -0500

    Update Main.java

--- TareaProyectoDiseniosoft/src/main/java/ec/edu/espol/tareaproyectodiseniosoft/Main.java
+++ TareaProyectoDiseniosoft/src/main/java/ec/edu/espol/tareaproyectodiseniosoft/Main.java
@@ -4,6 +4,8 @@
 
 package ec.edu.espol.tareaproyectodiseniosoft;
 
+import java.sql.Date;
+
 /**
  *
  * @author marsa
@@ -18,7 +20,7 @@ public class Main {
         unidad.actualizarEstado(EstadoUnidad.DISPONIBLE);
 
         MetodoPago metodo = new TarjetaCredit();
-        huesped.realizarReserva(sistema, unidad, new Date(), new Date(), metodo, 500.0);
+        huesped.realizarReserva(sistema, unidad, new Date(0, 0, 0), new Date(), metodo, 500.0);
 
         // Configurar chain de incidentes
         ManejadorIncidente anfitrion = new nfitrionIncidente();
