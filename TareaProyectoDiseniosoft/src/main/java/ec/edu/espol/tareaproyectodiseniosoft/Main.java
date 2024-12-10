ffc1f9808cef0915ab7d96093cd43c8f6bc1f439
DanielaReyes <dannreye@espol.edu.ec>
2024-12-09 23:46:02 -0500

    Daniela Reyes
    Main

--- TareaProyectoDiseniosoft/src/main/java/ec/edu/espol/tareaproyectodiseniosoft/Main.java
+++ TareaProyectoDiseniosoft/src/main/java/ec/edu/espol/tareaproyectodiseniosoft/Main.java
@@ -9,8 +9,26 @@ package ec.edu.espol.tareaproyectodiseniosoft;
  * @author marsa
  */
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Notificador notificador = new EmailNotificador();
+        SistemaHomeStay sistema = new SistemaHomeStay(notificador);
+
+        Huesped huesped = new Huesped();
+        Unidad unidad = new Unidad();
+        unidad.actualizarEstado(EstadoUnidad.DISPONIBLE);
+
+        MetodoPago metodo = new TarjetaCredit();
+        huesped.realizarReserva(sistema, unidad, new Date(), new Date(), metodo, 500.0);
+
+        // Configurar chain de incidentes
+        ManejadorIncidente anfitrion = new nfitrionIncidente();
+        ManejadorIncidente moderador = new ModeradorIncidente();
+        ManejadorIncidente soporteLegal = new SoporteLegalIncidente();
+
+        anfitrion.setSiguiente(moderador);
+        moderador.setSiguiente(soporteLegal);
+
+        // Reportar incidente
+        huesped.reportarIncidente("Incidente moderado", anfitrion);
     }
 }
