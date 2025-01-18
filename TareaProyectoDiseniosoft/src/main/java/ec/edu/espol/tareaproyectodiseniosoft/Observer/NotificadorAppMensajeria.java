a2b0f6428cf0cf17ba488206e35f2d0469806c36
MARLO VALLE <mcvalle@espol.edu.ec>
2025-01-17 23:08:57 -0500

    Update NotificadorAppMensajeria.java

--- TareaProyectoDiseniosoft/src/main/java/ec/edu/espol/tareaproyectodiseniosoft/Observer/NotificadorAppMensajeria.java
+++ TareaProyectoDiseniosoft/src/main/java/ec/edu/espol/tareaproyectodiseniosoft/Observer/NotificadorAppMensajeria.java
@@ -1,5 +1,9 @@
 package ec.edu.espol.tareaproyectodiseniosoft.Observer;
 
-public class NotificadorAppMensajeria {
+public class NotificadorAppMensajeria implements Observer {
+    @Override
+    public void actualizar(String mensaje) {
+        System.out.println("Enviando mensaje por la app de mensajería: " + mensaje);
+    }
 
 }
