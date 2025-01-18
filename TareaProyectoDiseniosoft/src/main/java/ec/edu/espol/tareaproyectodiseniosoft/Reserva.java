203067a413b346460f1cb474d6dd21940389a56e
MARLO VALLE <mcvalle@espol.edu.ec>
2025-01-17 22:42:56 -0500

    Update Reserva.java

--- TareaProyectoDiseniosoft/src/main/java/ec/edu/espol/tareaproyectodiseniosoft/Reserva.java
+++ TareaProyectoDiseniosoft/src/main/java/ec/edu/espol/tareaproyectodiseniosoft/Reserva.java
@@ -7,7 +7,11 @@ package ec.edu.espol.tareaproyectodiseniosoft;
 import ec.edu.espol.tareaproyectodiseniosoft.EstadoPago;
 import ec.edu.espol.tareaproyectodiseniosoft.FactoryMethod.Unidad;
 import ec.edu.espol.tareaproyectodiseniosoft.Huesped;
+
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Observer;
 
 /**
  *
@@ -19,20 +23,52 @@ public class Reserva {
     private Date fechafin;
     private Huesped huesped;
     private Unidad unidad;
-    private EstadoPago estado;
+
+    private List<Observer> observers = new ArrayList<>();
+    private String estado;
 
     public Reserva(Huesped huesped, Unidad unidad, Date fechaInicio, Date fechaFin) {
         this.huesped = huesped;
         this.unidad = unidad;
-        this.estado = EstadoPago.PENDIENTE;
+        this.estado = "PENDIENTE";
     }
 
     public Huesped getHuesped() {
         return huesped;
     }
 
-    public void setEstado(EstadoPago estado) {
+    public void setEstado(String estado) {
         this.estado = estado;
+    }
+
+    public Reserva(String estadoInicial) {
+        this.estado = estadoInicial;
+    }
+    
+    public String getEstado() {
+        return estado;
+    }
+
+    public void cambiarEstado(String nuevoEstado) {
+        this.estado = nuevoEstado;
+        notificarObservers("El estado de la reserva ha cambiado a: " + nuevoEstado);
+    }
+
+    @Override
+    public void agregarObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void quitarObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    @Override
+    public void notificarObservers(String mensaje) {
+        for (Observer observer : observers) {
+            observer.actualizar(mensaje);
+        }
     }
 
 }
