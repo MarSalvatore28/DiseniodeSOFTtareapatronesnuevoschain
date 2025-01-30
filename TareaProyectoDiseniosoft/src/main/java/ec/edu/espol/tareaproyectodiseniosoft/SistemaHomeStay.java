2004c0d7b95072bae12464aba9ab7652fa2173e2
Marlo Valle <152346851+MarSalvatore28@users.noreply.github.com>
2025-01-30 18:27:20 -0500

    3. Move Method
    Se movió realizarReserva de Huesped a SistemaHomeStay para reducir acoplamiento, mejorar cohesión y facilitar la extensibilidad.

--- TareaProyectoDiseniosoft/src/main/java/ec/edu/espol/tareaproyectodiseniosoft/SistemaHomeStay.java
+++ TareaProyectoDiseniosoft/src/main/java/ec/edu/espol/tareaproyectodiseniosoft/SistemaHomeStay.java
@@ -3,12 +3,15 @@ package ec.edu.espol.tareaproyectodiseniosoft;
 import ec.edu.espol.tareaproyectodiseniosoft.Observer.Notificador;
 import ec.edu.espol.tareaproyectodiseniosoft.Builder.Propiedad;
 import ec.edu.espol.tareaproyectodiseniosoft.FactoryMethod.Unidad;
-import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Date;
 
 public class SistemaHomeStay {
      private Notificador notificador;
+
+    public SistemaHomeStay() {
+    }
 
     public SistemaHomeStay(Notificador notificador) {
         this.notificador = notificador;
@@ -18,6 +21,19 @@ public class SistemaHomeStay {
         System.out.println("Buscando propiedades con filtro: " + filtro);
         return new ArrayList<>();
     }
+    
+    public Reserva realizarReserva(Huesped huesped, Unidad unidad, Date fechaInicio, Date fechaFin, MetodoPago metodo, double monto) {
+        if (unidad.getEstado() == EstadoUnidad.DISPONIBLE) {
+            Reserva reserva = crearReserva(huesped, unidad, fechaInicio, fechaFin);
+            unidad.actualizarEstado(EstadoUnidad.RESERVADA);
+            procesarPago(reserva, metodo, monto);
+            return reserva;
+        } else {
+            System.out.println("Unidad no disponible.");
+            return null;
+        }
+    }
+
 
     public Reserva crearReserva(Huesped huesped, Unidad unidad, java.util.Date fechaInicio, java.util.Date fechaFin) {
         System.out.println("Creando reserva para el huésped: " + huesped.getNombre());
