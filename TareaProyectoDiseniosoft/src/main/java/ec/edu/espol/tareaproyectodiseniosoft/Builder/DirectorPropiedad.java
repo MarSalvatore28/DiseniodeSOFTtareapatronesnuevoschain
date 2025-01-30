3a930a76286a88f7c34292a8e287f504c9844b29
Marlo Valle <152346851+MarSalvatore28@users.noreply.github.com>
2025-01-30 18:32:45 -0500

    4. Encapsulate Construction
    Se encapsuló la construcción en ApartamentoBuilder, VillaBuilder y CasaBuilder, reduciendo acoplamiento y mejorando cohesión en DirectorPropiedad."

--- TareaProyectoDiseniosoft/src/main/java/ec/edu/espol/tareaproyectodiseniosoft/Builder/DirectorPropiedad.java
+++ TareaProyectoDiseniosoft/src/main/java/ec/edu/espol/tareaproyectodiseniosoft/Builder/DirectorPropiedad.java
@@ -6,8 +6,8 @@ import ec.edu.espol.tareaproyectodiseniosoft.Usuario;
 import ec.edu.espol.tareaproyectodiseniosoft.FactoryMethod.Unidad;
 
 public class DirectorPropiedad {
-    public void construirApartamento(Builder builder) {
-        builder.reset();// Limpia el estado del builder para crear una nueva propiedad
+    public void construirApartamento(ApartamentoBuilder builder) {
+        builder.reset();
         builder.setId("AP001");
         builder.setNombre("Apartamento Lujo");
         builder.setDireccion("Calle Principal #123");
@@ -15,26 +15,23 @@ public class DirectorPropiedad {
         builder.setUnidades(List.of(new Unidad("U001"), new Unidad("U002")));
         builder.setAnfitrion(new Usuario("ANF001", "Juan Pérez"));
     }
-
-    public void construirVilla(Builder builder) {
+    public void construirVilla(VillaBuilder builder) {
         builder.reset();
         builder.setId("VL001");
         builder.setNombre("Villa de Campo");
         builder.setDireccion("Avenida Bosque #456");
-        builder.setReglas(List.of("Solo adultos", "No fiestas grandes"));// Reglas personalizadas para la villa.
+        builder.setReglas(List.of("Solo adultos", "No fiestas grandes"));
         builder.setUnidades(List.of(new Unidad("V001")));
-        builder.setAnfitrion(new Usuario("ANF002", "Carlos López"));// Define el anfitrión de la villa.
-    
+        builder.setAnfitrion(new Usuario("ANF002", "Carlos López"));
     }
-
-    public void construirCasa(Builder builder) {
+    public void construirCasa(CasaBuilder builder) {
         builder.reset();
         builder.setId("CS001");
         builder.setNombre("Casa Familiar");
         builder.setDireccion("Calle Jardín #789");
         builder.setReglas(List.of("Respetar vecinos", "No fiestas después de las 10 PM"));
-        builder.setUnidades(List.of(new Unidad("C001"), new Unidad("C002"), new Unidad("C003")));// Varias unidades disponibles.
+        builder.setUnidades(List.of(new Unidad("C001"), new Unidad("C002"), new Unidad("C003")));
         builder.setAnfitrion(new Usuario("ANF003", "Sofía Gómez"));
     }
-
 }
+
